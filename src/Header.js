e3ab6b954f5e7250bf50ad3bc9ab758d080e4e03
Cat <ccallcottstevens@gmail.com>
2021-06-01 22:26:50 +1000

    added styles to header with tailwind

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function Header() {
     return(
-        <>
-            <h1>Memeories</h1>
-        </>
+        <div>
+            <header className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 flex justify-around">
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+            alt="Problem?"    
+            />
+            <h1 className="text-6xl text-white">Meme Generator</h1>
+            </header>
+        </div>
     )
 }
 
