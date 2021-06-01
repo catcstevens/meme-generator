32b579bc06cf53a46fe7350ffc6832f86d1f94b6
Cat <ccallcottstevens@gmail.com>
2021-06-01 22:48:08 +1000

    initialised state in MemeGenerator

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -4,9 +4,23 @@ class MemeGenerator extends Component {
     constructor() {
         super()
         this.state = {
-
+            topText: '',
+            bottomText: '',
+            randomImage: 'http://i.imgflip.com/1bij.jpg',
+            allMemeImages: []
         }
     }
+componentDidMount() {
+    fetch('https://api.imgflip.com/get_memes')
+    .then(response => response.json())
+    .then(response => {
+        const {memes} = response.data
+        console.log(memes[0])
+        this.setState({allMemeImages: memes})
+    })
+    
+}
+
 render(){
     return (
             <h1>I will generate memes</h1>
