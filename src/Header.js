27b13bdf6210efd34e1a3f461f1e4c56791b6eb5
Cat <ccallcottstevens@gmail.com>
2021-06-01 16:36:53 +1000

    built files for header and meme generator

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <>
+            <h1>Memeories</h1>
+        </>
+    )
+}
+
+export default Header
---
+++ src/MemeGenerator.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class MemeGenerator extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+render(){
+    return (
+            <h1>I will generate memes</h1>
+        )
+}
+    
+}
+
+
+export default MemeGenerator
