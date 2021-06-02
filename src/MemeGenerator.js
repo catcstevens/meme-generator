071f1945c419d26dbd74ce881b8a8b9cc64071c6
Cat <ccallcottstevens@gmail.com>
2021-06-02 12:43:14 +1000

    created handleChange for input

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -9,21 +9,51 @@ class MemeGenerator extends Component {
             randomImage: 'http://i.imgflip.com/1bij.jpg',
             allMemeImages: []
         }
+        this.handleChange = this.handleChange.bind(this)
     }
 componentDidMount() {
     fetch('https://api.imgflip.com/get_memes')
     .then(response => response.json())
     .then(response => {
         const {memes} = response.data
-        console.log(memes[0])
         this.setState({allMemeImages: memes})
     })
     
 }
+handleChange(event) {
+    const {name, value} = event.target
+    this.setState({
+        [name]: value
+    })
+}
 
 render(){
     return (
-            <h1>I will generate memes</h1>
+        <div>
+        <form className='flex content-center p-10'>
+            <input 
+            type='text'
+            name='topText'
+            placeholder='Top Text'
+            value={this.state.topText} 
+            onChange={this.handleChange}
+            className='rounded-md border-4 border-pink-500'
+
+            />
+            <input 
+            type='text'
+            name='bottomText'
+            placeholder='Bottom Text'
+            value={this.state.bottomText} 
+            onChange={this.handleChange}
+            className='rounded-md border-4 border-pink-500'
+            />
+
+            <button className='rounded-md bg-pink-500 text-white font-bold px-8 py-1'> Gen </button>
+        </form>
+
+        </div>
+            
         )
 }
     
