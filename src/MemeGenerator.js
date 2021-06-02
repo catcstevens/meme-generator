7aa3469def27d97255e86b4c98581dbdd9c71c59
Cat <ccallcottstevens@gmail.com>
2021-06-02 13:10:04 +1000

    finished the random meme generator

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -10,6 +10,7 @@ class MemeGenerator extends Component {
             allMemeImages: []
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 componentDidMount() {
     fetch('https://api.imgflip.com/get_memes')
@@ -27,17 +28,29 @@ handleChange(event) {
     })
 }
 
+handleSubmit(event) {
+    event.preventDefault()
+    const randNum = Math.floor(Math.random() *this.state.allMemeImages.length)
+    const randMemeImage = this.state.allMemeImages[randNum].url 
+    this.setState({
+        randomImage: randMemeImage
+    })
+
+}
+
 render(){
     return (
         <div>
-        <form className='flex content-center p-10'>
+        <form className='flex content-center p-10'
+        onSubmit={this.handleSubmit}
+        >
             <input 
             type='text'
             name='topText'
             placeholder='Top Text'
             value={this.state.topText} 
             onChange={this.handleChange}
-            className='rounded-md border-4 border-pink-500'
+            className='rounded-md border-4 border-pink-500 text-lg'
 
             />
             <input 
@@ -46,11 +59,20 @@ render(){
             placeholder='Bottom Text'
             value={this.state.bottomText} 
             onChange={this.handleChange}
-            className='rounded-md border-4 border-pink-500'
+            className='rounded-md border-4 border-pink-500 text-lg'
             />
 
-            <button className='rounded-md bg-pink-500 text-white font-bold px-8 py-1'> Gen </button>
+            <button 
+            className='rounded-md bg-pink-500 text-white font-bold px-8 py-1 text-xl'
+            on
+            > Gen 
+            </button>
         </form>
+        <div className='meme'>
+            <img src={this.state.randomImage} alt=''/>
+            <h2 className='absolute inset-x-0 top-0'>{this.state.topText}</h2>
+            <h2 className='absolute bottom-0'>{this.state.bottomText}</h2>
+        </div>
 
         </div>
             
